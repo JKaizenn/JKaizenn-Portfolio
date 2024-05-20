6989e39d381e3a6a4dbe074dda9911a24ef61172
Jessen Forbush <for21011@byui.edu>
2024-05-20 11:06:50 -0600

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get the last modified date of the document
+const lastModifiedDate = document.lastModified;
+document.getElementById("lastModified").textContent =
+"Last modified: " + lastModifiedDate;
